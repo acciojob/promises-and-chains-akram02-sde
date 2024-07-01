3c37289537ee2e926ebfccf9a54d88fc031569bf
Akram Ahmed Khan <68415682+akram02-sde@users.noreply.github.com>
2024-07-01 20:06:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+
+
+form.addEventListener('submit',(e)=>{
+	e.preventDefault()
+	checkAge(age.value, name.value).then(data=>alert(data))
+							.catch(data=>alert(data))
+})
+
+
+async function checkAge(age,name){
+	return new Promise((res, rej)=>{
+		if(age>18){
+			setTimeout(()=>res(`Welcome, ${name}. You can vote.`),4000)
+		}
+		else{
+			setTimeout(()=>res(`Oh sorry ${name}. You aren't old enough.`),4000)
+		}
+	})
+}
